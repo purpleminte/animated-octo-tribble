1cbbd31689f0936bc2c917edc0df9d465c605120
purpleminte <131814250+purpleminte@users.noreply.github.com>
2023-05-02 15:39:07 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int recursiveFactorial(int N)
+{
+    if (N == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        //the function calls itself:
+        return (N * recursiveFactorial(N - 1));
+    }
+}
+
+int hanoi(int N)
+{
+    if (N <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (hanoi(N-1)*2 + 1);
+    }
+}
+
+int main()
+{
+    int tower;
+    cout << "How tall is the tower? -> ";
+    cin >> tower;
+    cout << endl << "The tower takes [" << hanoi(tower) << "] steps to completely move." << endl;
+}
+
+// -   1   2   3   4   5   6   7
+
+//a.. ... ... ... ... ... ... ..a
+//b.. b.. ... .a. .a. ... ..b ..b
+//c.. c.a cba cb. .bc abc a.c ..c
+
+//     a > b > a > c > a > b > a > fin
